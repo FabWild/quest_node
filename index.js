a5ec09fa2425a4f9add80ab24b2df56f52e42959
FabWild <fab@dfweb.fr>
2017-10-16 15:13:06 +0200

    test deux pages node

---
+++ index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const express = require('express')
+const app = express()
+
+var path = require("path");
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+app.get('/about',function(req,res){
+  res.sendFile(path.join(__dirname+'/about.html'));
+});
+
+app.listen(3000);
